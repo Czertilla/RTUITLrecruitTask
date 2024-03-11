f280da28630540dcc0bad8a513be327f90d40c6f
Czertilla <blaztermapskorsak@gmail.com>
2024-03-12 01:24:53 +0300

    chore(app/contextmanager) add new derective add schemas.dynamic.DynamicModels.generate     to func startup

--- app/contextmanager.py
+++ app/contextmanager.py
@@ -1,15 +1,15 @@
 from fastapi.concurrency import asynccontextmanager
-from .app import FastAPI
-__all__ = []
+from schemas.dynamic import DynamicModels
+
 
 async def startup():
-    ...
+    await DynamicModels.generate()
 
 async def shutdown():
     ...
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(app):
     await startup()
     yield 
     await shutdown()
