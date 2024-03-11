3d3508518ce0df365cf334972a326472642b96aa
Czertilla <blaztermapskorsak@gmail.com>
2024-03-12 00:58:36 +0300

    feature(database/models) add camerus module add class DependenciesOrm

---
+++ database/models/camerus.py
@@ -0,0 +1,16 @@
+from uuid import uuid4
+from sqlalchemy import ForeignKey, PickleType, Table
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID
+from .base import Base
+from typing import Any
+
+class DependenciesOrm(Base):
+    __tablename__ = "dependencies"
+
+    key: Mapped[str]
+    value_type: Mapped[str]
+    enclosure: Mapped[UUID|None] = mapped_column(ForeignKey("dependencies.ID"))
+    destination: Mapped[str|None]
+    field_attrs: Mapped[dict[str, Any]] = mapped_column(default={})
+
