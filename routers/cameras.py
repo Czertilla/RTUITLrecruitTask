9dbddf9610781c3e8c799a9e8e0b21c5a51fc7f0
Czertilla <blaztermapskorsak@gmail.com>
2024-03-12 01:28:15 +0300

    feature(routers/cameras) realize send_case func

--- routers/cameras.py
+++ routers/cameras.py
@@ -1,6 +1,13 @@
-from fastapi import APIRouter
-from schemas.cameras import SCameraRegist, camerus1
+from fastapi import APIRouter, HTTPException
+from fastapi.encoders import jsonable_encoder
+from schemas.cameras import SCameraRegist, SCameraCase
+from schemas.dynamic import DynamicModels
 from uuid import UUID, uuid4
+from pydantic import ValidationError
+from utils.requests import deserialize
+from logging import getLogger
+
+logger = getLogger(__name__)
 
 cameras = APIRouter(prefix="/cameras")
 
@@ -9,5 +16,11 @@ def regist(request: SCameraRegist) -> UUID:
     return uuid4()
 
 @cameras.post("/case")
-async def send_case(request: camerus1) -> None:
-    ...
+async def send_case(request: SCameraCase) -> None:
+    metadata = await deserialize(request.metadata)
+    model = await DynamicModels.validate(metadata)
+    if type(model) is dict:
+        raise HTTPException(status_code=422, detail=jsonable_encoder(model))
+    print(model)
+    
+    
