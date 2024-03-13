ce446e001f6977ca6e8438a83ad0ca4025c7fcec
Czertilla <blaztermapskorsak@gmail.com>
2024-03-13 15:29:28 +0300

    refactor(schemas/cameras) remove BaseCamerus class

--- schemas/cameras.py
+++ schemas/cameras.py
@@ -1,22 +1,18 @@
 from pydantic import BaseModel
 from typing import Annotated
-from fastapi import Query, UploadFile
-from uuid import UUID
-from datetime import datetime
-
-class BaseCamerus(BaseModel):
-    ...
+from fastapi import Depends, Query, UploadFile
+from .dynamic import DynamicModels
 
 class SCoordsGPS(BaseModel):
     latitude: Annotated[float, Query(ge= -90, le=90, description="GPS latitude")]
     longitude: Annotated[float, Query(ge= -180, le=180, description="GPS longitude")]
 
+print("schemas")
 class SCameraRegist(BaseModel):
     GPScoords: Annotated[SCoordsGPS, Query(title="GPS coordinates")]
-    CameraType: Annotated[str, Query(pattern='|'.join([cls.__name__ for cls in BaseCamerus.__subclasses__()]))]
+    CameraType: Annotated[str, Depends(DynamicModels.get_pattern)]
     description: Annotated[str, Query(max_length=100)]
 
 class SCameraCase (BaseModel):
     # photo: UploadFile
-    metadata: dict
-
+    metadata: bytes
