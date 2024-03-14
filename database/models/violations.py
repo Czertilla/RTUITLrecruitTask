f5c3a2876deb55d590e032729becb0c7cff3ff9c
Czertilla <blaztermapskorsak@gmail.com>
2024-03-14 12:26:10 +0300

    feature(database/models) add violations module add class ViolationORM

---
+++ database/models/violations.py
@@ -0,0 +1,8 @@
+from .base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+
+class ViolationORM(Base):
+    __tablename__ = "violations"
+
+    violation_type: Mapped[str] = mapped_column(unique=True)
+    fine: Mapped[float]
