a74056285e757cd5f21bb2bb9406d4c9f6155c12
Czertilla <blaztermapskorsak@gmail.com>
2024-03-14 10:00:15 +0300

    routers(cameras) add realisation to regist func

--- routers/cameras.py
+++ routers/cameras.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, HTTPException
 from fastapi.encoders import jsonable_encoder
 from schemas.cameras import SCameraRegist, SCameraCase
 from schemas.dynamic import DynamicModels
-from uuid import UUID, uuid4
+from database.repositories import CameraRepo
+from uuid import UUID
 from pydantic import ValidationError
 from utils.requests import deserialize
 from logging import getLogger
@@ -12,8 +13,13 @@ logger = getLogger(__name__)
 cameras = APIRouter(prefix="/cameras")
 
 @cameras.post("/regist")
-def regist(request: SCameraRegist) -> UUID:
-    return uuid4()
+async def regist(request: SCameraRegist) -> UUID:
+    return (await CameraRepo.regist(
+        latitude=request.GPScoords.latitude,
+        longitude=request.GPScoords.longitude,
+        description=request.description,
+        cam_type=request.CameraType
+    ))
 
 @cameras.post("/case")
 async def send_case(request: SCameraCase) -> None:
@@ -21,6 +27,5 @@ async def send_case(request: SCameraCase) -> None:
     model = await DynamicModels.validate(metadata)
     if type(model) is dict:
         raise HTTPException(status_code=422, detail=jsonable_encoder(model))
-    print(model)
     
     
