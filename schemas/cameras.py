bc936f6c933340c451a23d5624965912af728f4e
Czertilla <blaztermapskorsak@gmail.com>
2024-03-14 10:01:00 +0300

    feature(schemas/cameras) add check_camera_type class SCameraRegist:     add classmethod check_camera_type

--- schemas/cameras.py
+++ schemas/cameras.py
@@ -1,18 +1,23 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError, validator
 from typing import Annotated
-from fastapi import Depends, Query, UploadFile
+from fastapi import Depends, Query
 from .dynamic import DynamicModels
 
 class SCoordsGPS(BaseModel):
     latitude: Annotated[float, Query(ge= -90, le=90, description="GPS latitude")]
     longitude: Annotated[float, Query(ge= -180, le=180, description="GPS longitude")]
 
-print("schemas")
 class SCameraRegist(BaseModel):
     GPScoords: Annotated[SCoordsGPS, Query(title="GPS coordinates")]
-    CameraType: Annotated[str, Depends(DynamicModels.get_pattern)]
+    CameraType: Annotated[str, Query()]
     description: Annotated[str, Query(max_length=100)]
 
+    @validator("CameraType")
+    def check_camera_type(cls, value):
+        if value not in (pattern:=DynamicModels.camerus_pattern_list):
+            raise ValueError(f"CameraType can be only {'|'.join(pattern)}")
+        return value
+
 class SCameraCase (BaseModel):
-    # photo: UploadFile
+    # photo: UploadFilee
     metadata: bytes
