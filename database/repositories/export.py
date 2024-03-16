954ba4d6261ff1a437157df7b8e5c9d654e99651
Czertilla <blaztermapskorsak@gmail.com>
2024-03-16 21:36:02 +0300

    feature(database/repositories) add export module add class ExportRepo:     add method filt     add method export

---
+++ database/repositories/export.py
@@ -0,0 +1,50 @@
+from fastapi import Depends
+from database.models.base import Base
+from database.engine import new_session
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+import inspect
+
+class ExportRepo:
+    orm = Base
+
+    @classmethod
+    def filt(cls, kwargs)-> tuple:
+        return tuple(set(kwargs) - set(cls.orm.__annotations__.keys()))
+
+    @classmethod
+    async def export(cls, data: list[dict], unique_column_name) -> dict:
+        badkeys=cls.filt(data[0])
+        invalid_params = [f"invalid colums names: {badkeys}, have been ignored"]
+        updated_params = []
+        count = 0
+        for record in data:
+            [record.pop(key) for key in badkeys]
+            async with new_session() as session:
+                model = (
+                    await session.execute(
+                        select(cls.orm).
+                        where(getattr(cls.orm, unique_column_name) == record.get(unique_column_name))
+                    )
+                ).scalar_one_or_none()
+            if model is None:
+                model = cls.orm(**record)
+                count += 1
+                updated = False
+            else:
+                for key, val in record.items():
+                    setattr(model, key, val)
+                updated_params.append(str(tuple(record.values())+(model.ID,)))
+                updated = False
+            try:
+                async with new_session() as session:
+                    await session.merge(model)
+                    await session.commit()
+            except Exception as e:
+                invalid_params.append(f"{e.statement}, ({e.params}) : {e.orig.args[0]}")
+                if updated:
+                    updated_params.pop(-1)
+                else:
+                    count -= 1
+        return {"added": count, "updated": updated_params, "invalid": invalid_params}
+
