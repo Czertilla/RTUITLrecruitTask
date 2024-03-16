f04a7a2cca2296830872853fa83368ada5d68d79
Czertilla <blaztermapskorsak@gmail.com>
2024-03-16 21:37:23 +0300

    chore(utils/importer) add exceptions handling replacing nan to None data

--- utils/importer.py
+++ utils/importer.py
@@ -1,8 +1,9 @@
 
 from io import BytesIO
 from pandas import DataFrame, read_excel
-
+from numpy import nan
 
 def import_excel(data: bytes) -> list[dict]:
     frame = read_excel(BytesIO(data))
+    frame.replace(nan, None, inplace=True)
     return frame.to_dict("records")
