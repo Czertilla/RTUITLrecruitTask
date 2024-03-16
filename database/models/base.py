b0686258cc98acd88ccf3ee8ebd4ff9e1ec869ac
Czertilla <blaztermapskorsak@gmail.com>
2024-03-16 21:54:15 +0300

    feature(database/models/base) add IdMinxin class

--- database/models/base.py
+++ database/models/base.py
@@ -1,14 +1,17 @@
 from typing import Any
 from uuid import uuid4, UUID
 from sqlalchemy.types import JSON
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, column_property
+from sqlalchemy.ext.declarative import declared_attr
 
+class IdMinxin:
+    @declared_attr
+    def ID(cls) -> Mapped[UUID]:
+        return mapped_column(primary_key=True, default=uuid4)
 
-class Base(DeclarativeBase):
+class Base(DeclarativeBase, IdMinxin):
     __abstract__ = True
-
-    ID: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
-
+    
     type_annotation_map = {
         dict[str, Any]: JSON
     }
