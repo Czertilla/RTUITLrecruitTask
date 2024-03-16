364de1884ed97e941b1e5057d4b058cdefd579ab
Czertilla <blaztermapskorsak@gmail.com>
2024-03-16 22:30:54 +0300

    debug(database/models/cases) class CaseORM:     change mapped type from DateTime to datetime     change foreign key reference from carowmers to car_owners

--- database/models/cases.py
+++ database/models/cases.py
@@ -1,16 +1,17 @@
-from sqlalchemy import DateTime, ForeignKey
+from sqlalchemy import ForeignKey
 from .base import Base
 from uuid import UUID
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
 
 class CaseORM(Base):
     __tablename__ = "cases"
 
     transport: Mapped[str]
-    owner_id: Mapped[UUID|None] = mapped_column(ForeignKey("carowners.ID"))
+    owner_id: Mapped[UUID|None] = mapped_column(ForeignKey("car_owners.ID"))
     camera_id: Mapped[UUID] = mapped_column(ForeignKey("cameras.ID"))
     violation_id: Mapped[UUID] = mapped_column(ForeignKey("violations.ID"))
     violation_value: Mapped[str] = mapped_column(default='')
     skill_value: Mapped[int]
-    datetime: Mapped[DateTime]
+    datetime: Mapped[datetime]
 
