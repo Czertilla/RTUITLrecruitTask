f023b2225707e58b84469a5b449a103787e09f04
Czertilla <blaztermapskorsak@gmail.com>
2024-03-16 21:38:15 +0300

    feature(routers/managers) add import_from_xlsx func add endpoint import_from_xlsx

--- routers/managers.py
+++ routers/managers.py
@@ -1,4 +1,25 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, UploadFile
+from logging import getLogger
 
-managers = APIRouter()
+from pydantic import Field
+from database.repositories.export import ExportRepo
+from schemas.managers import SImportExelRespose
+from utils.importer import import_excel
 
+logger = getLogger(__name__)
+
+managers = APIRouter(prefix="/managers")
+
+export_repo_dep = {repo.orm.__tablename__: repo for repo in ExportRepo.__subclasses__()}
+export_tables_pattern = '|'.join(export_repo_dep.keys())
+
+@managers.post("/import/{table}/upload-file")
+async def import_from_xlsx(
+    table: Annotated[str, Field(pattern=export_tables_pattern)],
+    file: UploadFile
+) -> SImportExelRespose:
+    repository = export_repo_dep.get(table)
+    data = import_excel(await file.read())
+    success = (await repository.export(data))
+    return success
