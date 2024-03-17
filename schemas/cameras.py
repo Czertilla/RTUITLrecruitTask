fedc12c02ef1967e0d8a93b6d1ada6675a1cd88a
Czertilla <blaztermapskorsak@gmail.com>
2024-03-17 22:02:21 +0300

    feature(schemas/cameras) add Camerus class add class Camerus:     add clsmethod parse_camerus1     add clsmethod parse_camerus2     add clsmethod parse_camerus3     add clsmethod parse

--- schemas/cameras.py
+++ schemas/cameras.py
@@ -1,7 +1,75 @@
-from pydantic import BaseModel, validator
-from typing import Annotated
+from datetime import datetime
+from uuid import UUID
+from pydantic import BaseModel, Field, validator
+from typing import Annotated, Type
 from fastapi import Query, UploadFile
-from .dynamic import DynamicModels 
+
+from database.repositories.violations import ViolationRepo
+from schemas.dynamic import DynamicModels 
+from database.repositories import CameraRepo
+from asyncio import run
+
+class Camerus:
+    @classmethod
+    def parse_camerus1(cls, data: dict) -> tuple:
+        numbers = data.get("transport_numbers")
+        return (
+            f"{numbers[0]}{data.get('transport_chars')}{numbers[1:]}{data.get('transport_region')}",
+            data.get("camera_id"),
+            data.get("violation_id"),
+            data.get("violation_value"),
+            data.get('skill_value'),
+            data.get('datetime')
+        )  
+
+    @classmethod
+    def parse_camerus2(cls, data: dict) -> tuple:
+        transport:dict = data.get("transport")
+        numbers = transport.get("numbers")
+        camera:dict = data.get('camera')
+        violation: dict = data.get('violation')
+        skill: dict = data.get('skill')
+        return (
+            f"{numbers[0]}{transport.get('chars')}{numbers[1:]}{data.get('region')}",
+            camera.get("id"),
+            violation.get('id'),
+            violation.get('value'),
+            skill.get('value'),
+            datetime(**data.get('datetime'))
+        )
+    
+    @classmethod
+    def parse_camerus3(cls, data: dict) -> tuple:
+        camera:dict = data.get('camera')
+        violation: dict = data.get('violation')
+        return (
+            data.get('transport'),
+            camera.get("id"),
+            violation.get('id'),
+            violation.get('value'),
+            data.get('skill'),
+            datetime.fromtimestamp(int(data.get('datetime')))
+        )
+
+    @classmethod
+    async def parse(cls, model: Type[BaseModel], model_name: str) -> dict:
+        values = getattr(cls, f"parse_{model_name}")(model.model_dump())
+        keys = (
+            "transport",
+            "camera_id",
+            "violation_id",
+            "violation_value",
+            'skill_value',
+            'case_timestamp'
+        )
+        return {
+            key:value 
+            for 
+                key, value
+            in 
+                zip(keys, values)
+        }
+
 
 class SCoordsGPS(BaseModel):
     latitude: Annotated[float, Query(ge= -90, le=90, description="GPS latitude")]
@@ -20,4 +88,29 @@ class SCameraRegist(BaseModel):
 
 class SCameraCase (BaseModel):
     photo: UploadFile
-    metadata: bytes
+    metadata: UploadFile
+
+class SCaseInsert (BaseModel):
+    transport: Annotated[str, Field(pattern="[0-9][A-Z]{3}[0-9]{4}")]
+    photo_id: UUID
+    camera_id: UUID
+    violation_id: UUID
+    violation_value: Annotated[str, Field(max_length=100)]
+    skill_value: Annotated[int, Field(ge=1, le=100)]
+    case_timestamp: datetime
+
+    @validator("case_timestamp")
+    def ensure_time_range(cls, value: datetime):
+        if not value.timestamp() < datetime.now().timestamp():
+            raise ValueError("the incident happened in the future")
+        return value
+    
+    # @validator("camera_id")
+    # async def check_camera_existence(cls, value: UUID):
+    #     if not (await CameraRepo.check_existence(value)):
+    #         raise ValueError(f"camera with ID {value} doesn`t exist in database")
+    
+    # @validator("violation_id")
+    # async def check_violation_existence(cls, value: UUID):
+    #     if not  (await ViolationRepo.check_existence(value)):
+    #         raise ValueError(f"violation with ID {value} doesn`t exist in database")
