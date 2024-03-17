709f0fcdb28d58b0ce6073f3998c964707ccd0c0
Czertilla <blaztermapskorsak@gmail.com>
2024-03-17 22:10:31 +0300

    feature(cls ViolationRepo) add functions class database.repositories.ViolationRepo:     add clsmethod find_by_id     add clsmethod check_existence

--- database/repositories/violations.py
+++ database/repositories/violations.py
@@ -1,7 +1,6 @@
-from pandas import DataFrame
+from uuid import UUID
+from sqlalchemy import select
 from database.engine import new_session
-from typing import Type
-from database.models.base import Base
 from database.models import ViolationORM
 from .export import ExportRepo
 
@@ -11,3 +10,18 @@ class ViolationRepo(ExportRepo):
     @classmethod
     async def export(cls, data: list[dict]) -> dict:
         return await super().export(data, "violation_type")
+
+    @classmethod
+    async def find_by_id(cls, ID: UUID) -> ViolationORM:
+        async with new_session() as session:
+            violation = (
+                await session.execute(
+                    select(ViolationORM).
+                    where(ViolationORM.ID == ID)
+                )
+            ).scalar_one_or_none()
+        return violation
+
+    @classmethod
+    async def check_existence(cls, ID: UUID) -> bool:
+        return (await cls.find_by_id(ID)) is not None
