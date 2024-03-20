705245556ec314763e3b5b7324b1cc8d8a68b841
Czertilla <blaztermapskorsak@gmail.com>
2024-03-20 22:13:42 +0300

    feature(routers) add fastapi_users routers add router auth_router add router register_router add router reset_password_router add router users_router

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,14 +1,43 @@
 from fastapi import FastAPI
+from app.auth.auth import fastapi_users, auth_backend
+from schemas.auth import UserCreate, UserRead, UserUpdate
 from .cameras import cameras
 from .specialists import specialists
 from .managers import managers
-
 routers = (
     cameras,
     specialists,
-    managers
+    managers,
 )
 
 def include_routers(app: FastAPI) -> None:
     for router in routers:
         app.include_router(router)
+
+    app.include_router(
+        fastapi_users.get_auth_router(auth_backend),
+        prefix="/auth/jwt",
+        tags=["auth"],
+    )
+
+    app.include_router(
+        fastapi_users.get_register_router(UserRead, UserCreate),
+        prefix="/auth",
+        tags=["auth"],
+    )
+
+    app.include_router(
+        fastapi_users.get_reset_password_router(),
+        prefix="/auth",
+        tags=["auth"],
+    )
+
+    app.include_router(
+        fastapi_users.get_users_router(
+            UserRead, 
+            UserUpdate, 
+            requires_verification=True
+        ),
+        prefix="/users",
+        tags=["users"],
+    )
