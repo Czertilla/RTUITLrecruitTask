4d0cbf02bed6946b1181a9f6d76c6d0d790ffbce
Czertilla <blaztermapskorsak@gmail.com>
2024-03-20 22:16:02 +0300

    feature(db/rep/cases) add get_current_case,.. func class CaseRepo:     add clsmthd get_current_case     add clsmthd get_random_case     add clsmthd vote

--- database/repositories/__init__.py
+++ database/repositories/__init__.py
@@ -2,3 +2,4 @@ from .camerus import CamerusRepo
 from .cameras import CameraRepo
 from .carowners import CarOwnerORM
 from .violations import ViolationORM
+from .files import FileRepo
--- database/repositories/cases.py
+++ database/repositories/cases.py
@@ -1,7 +1,9 @@
+from typing import TYPE_CHECKING
 from database.models.cases import CaseORM
 from database.engine import new_session
 from schemas.cameras import SCaseInsert
-from datetime import datetime, UTC, timezone
+from sqlalchemy import select
+from database.models import UserORM
 
 
 class CaseRepo:
@@ -12,3 +14,34 @@ class CaseRepo:
             session.add(CaseORM(**record))
             await session.flush()
             await session.commit()
+    
+    @classmethod
+    async def get_current_case(cls, user: UserORM) -> CaseORM|None:
+        for vote in user.case_associations:
+            if vote.justify is None:
+                return vote.case_table
+
+    @classmethod
+    async def get_random_case(cls, user: UserORM) -> CaseORM|None:
+        if (case_data:= await cls.get_current_case(user)) is None:
+            stmt = (
+                select(CaseORM).
+                where(
+                    CaseORM.skill_value == user.skill,
+                    user not in CaseORM.users
+                )
+            )
+        else:
+            return case_data
+        user.cases.append(case_data)
+        async with new_session() as session:
+            result = await session.execute(stmt)
+        return result.scalar_one_or_none()
+
+    @classmethod
+    async def vote(cls, user: UserORM, vote: str) -> str:
+        if (case_data := await cls.get_current_case(user)) is None:
+            return "case not selected"
+
+        
+        
