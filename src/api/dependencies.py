69cb85855eb66ced963aaea0b19d4e2925b32f4c
Czertilla <blaztermapskorsak@gmail.com>
2024-03-23 23:56:27 +0300

    debug(api.dependencies) rename same functions endless function

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from .routers import routers
-from .auth.routers import include_routers
+from .auth.routers import include_routers as include_auth_routers
 
 
 def include_routers(app: FastAPI) -> None:
     for router in routers:
         app.include_router(router)
-    include_routers(app)
+    include_auth_routers(app)
