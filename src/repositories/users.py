ab9223e972bd21004053fb865de50f3982cd05cf
Czertilla <blaztermapskorsak@gmail.com>
2024-03-26 00:01:53 +0300

    feature(repositories) update UserRepo class module users:     update class UserRepo:         remove method merge         update method check_username         add method set_manager

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -2,7 +2,8 @@ from typing import AsyncGenerator
 from uuid import UUID
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 from fastapi import Depends
 from database import new_session, BaseRepo
 from logging import getLogger
@@ -20,31 +21,46 @@ async def get_user_db(session: AsyncSession = Depends(get_async_session)):
     yield SQLAlchemyUserDatabase(session, UserORM)
 
 class UserRepo(BaseRepo):
-    async def merge(cls, model: UserORM):
-        async with new_session() as session:
-            await session.merge(model)
-            await session.flush()
-            await session.commit()
+    model = UserORM
 
-
-    async def check_username(cls, value) -> bool:
-        async with new_session() as session:
-            user = (
-                await session.execute(
-                    select(UserORM).
-                    where(UserORM.username == value)
-                )
-            ).scalar_one_or_none()
+    async def check_username(self, value) -> bool:
+        user = (
+            await self.execute(
+                select(UserORM).
+                where(UserORM.username == value)
+            )
+        ).scalar_one_or_none()
         return user is not None
 
-    async def verify(cls, id: UUID) -> UserORM|None:
-        user = await cls.find_by_id(id)
+    async def verify(self, id: UUID) -> UserORM|None:
+        user = await self.find_by_id(id)
         if user is None:
             logger.error("user %s not found", id)
             logger.warning("%s user not verified", id)
             return
         user.is_verified = True
         user.role = "specialist"
-        await cls.merge(user)
+        await self.merge(user)
         return user
+    
+    async def get(self, id: UUID) -> UserORM:
+        stmt = (
+            select(UserORM). 
+            where(UserORM.id == id).
+            options(selectinload(UserORM.votes_list), selectinload(UserORM.cases_list))
+        )
+        return await self.session.scalar(stmt)
+    
+    async def set_manager(self, id: UUID):
+        user = await self.find_by_id(id)
+        if user is None:
+            logger.error("user %s not found", id)
+            logger.warning("superuser status for user %s is not set", id)
+            return
+        user.is_superuser = True
+        user.is_active = True
+        user.is_verified = True
+        user.role = "manager"
+        await self.merge(user)
+
 
