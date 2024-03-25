f9f8e739f6de2b67d4a7a06c136f3be086fccaf1
Czertilla <blaztermapskorsak@gmail.com>
2024-03-26 00:00:12 +0300

    feature(repositories) add find_new_for_vote func module cases:     update class CaseRepo:         add method find_new_for_vote

--- src/repositories/cases.py
+++ src/repositories/cases.py
@@ -1,46 +1,31 @@
-from models.cases import CaseORM
-from database import new_session
-from repositories.users import UserRepo
+from uuid import UUID
+from models.cases import CaseORM, CaseStatus
 from sqlalchemy import select
 from sqlalchemy.orm import selectinload
-from random import randint
 from database import BaseRepo
 
 class CaseRepo(BaseRepo):
     model = CaseORM
 
-    # async def get_current_case(self, user: UserORM) -> CaseORM|None:
-    #     for vote in user.votes_list:
-    #         if vote.justify is None:
-    #             return vote.case_model
 
-
-    # async def get_random_case(self, user: UserORM) -> CaseORM|None:
-    #     async with new_session() as session:
-    #         user = await session.scalar(
-    #             select(UserORM). 
-    #             where(UserORM.id == user.id).
-    #             options(selectinload(UserORM.votes_list), selectinload(UserORM.cases_list))
-    #         )
-    #     if (case_data:= await self.get_current_case(user)) is None:
-    #         stmt = (
-    #             select(CaseORM).
-    #             where(
-    #                 CaseORM.skill_value == user.skill
-    #             ).
-    #             options(selectinload(CaseORM.votes_list), selectinload(CaseORM.users_list))
-    #         )
-    #     else:
-    #         return case_data
-    #     async with new_session() as session:
-    #         result = await session.execute(stmt)
-    #     cases_list:list = result.scalars().all()
-    #     while len(cases_list) > 0:
-    #         case_data: CaseORM = cases_list.pop(randint(0, len(cases_list)-1))
-    #         if user not in case_data.users_list:
-    #             user.cases_list.append(case_data)
-    #             UserRepo.merge(user)
-    #             return case_data
+    async def find_new_for_vote(self, skill: int, user_id: UUID) -> list[model]:
+        stmt = (
+            select(self.model).
+            where(
+                self.model.skill_value == skill,
+                self.model.status == CaseStatus.INITIATED
+            ).
+            # options(
+            #     selectinload(self.model.users_list)
+            # ).
+            filter_by(
+                all(
+                    self.model.users_list, 
+                    lambda x: x.id != user_id
+                )
+            )
+        )
+        return (await self.execute(stmt)).scalars().all()
 
 
     # async def vote(self, user: UserORM, vote: str) -> str:
