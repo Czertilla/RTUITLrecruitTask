beac1f1aa99fce7e3f3a356d65cd917655bcaa05
Czertilla <blaztermapskorsak@gmail.com>
2024-03-26 00:03:51 +0300

    feautre(services) update CaseService class module cases:     update class CaseService:         add method get_current_case (uncomment)         add method get_random_case (uncomment)         add method vote (uncomment)

--- src/services/cases.py
+++ src/services/cases.py
@@ -1,9 +1,11 @@
 from uuid import UUID
+from models.cases import CaseORM
+from models.users import UserORM
 from schemas.cameras import SCameraCase, SCaseInsert
 from services.dynamic import CamerusService
-from units_of_work.case import CaseUOW
 from utils.absract.service import BaseService
 from utils.requests import deserialize
+from random import choice
 
 
 class CaseService(BaseService):
@@ -28,41 +30,26 @@ class CaseService(BaseService):
         
 
 
-    # async def get_current_case(self, user: UserORM) -> CaseORM|None:
-    #     for vote in user.votes_list:
-    #         if vote.justify is None:
-    #             return vote.case_model
+    async def get_current_case(self, user: UserORM) -> CaseORM|None:
+        for vote in user.votes_list:
+            if vote.justify is None:
+                return vote.case_model
 
-    # @classmethod
-    # async def get_random_case(self, user: UserORM) -> CaseORM|None:
-    #     async with new_session() as session:
-    #         user = await session.scalar(
-    #             select(UserORM). 
-    #             where(UserORM.id == user.id).
-    #             options(selectinload(UserORM.votes_list), selectinload(UserORM.cases_list))
-    #         )
-    #     if (case_data:= await self.get_current_case(user)) is None:
-    #         stmt = (
-    #             select(CaseORM).
-    #             where(
-    #                 CaseORM.skill_value == user.skill
-    #             ).
-    #             options(selectinload(CaseORM.votes_list), selectinload(CaseORM.users_list))
-    #         )
-    #     else:
-    #         return case_data
-    #     async with new_session() as session:
-    #         result = await session.execute(stmt)
-    #     cases_list:list = result.scalars().all()
-    #     while len(cases_list) > 0:
-    #         case_data: CaseORM = cases_list.pop(randint(0, len(cases_list)-1))
-    #         if user not in case_data.users_list:
-    #             user.cases_list.append(case_data)
-    #             UserRepo.merge(user)
-    #             return case_data
 
-    # @classmethod
-    # async def vote(self, user: UserORM, vote: str) -> str:
-    #     if (case_data := await self.get_current_case(user)) is None:
-    #         return "case not selected"
-    #     return "OK"
+    async def get_random_case(self, user: UserORM) -> CaseORM|None:
+        async with self.uow:
+            user = await self.uow.users.get(user.id)
+            if (case_data:= await self.get_current_case(user)) is None:
+                cases_list = await self.uow.cases.find_new_for_vote(user)
+            else:
+                return case_data
+            case_model: CaseORM = choice(cases_list)
+            case_model.users_list.append(user)
+            await self.uow.cases.merge(case_model, flush=True)
+        return case_model
+
+
+    async def vote(self, user: UserORM, vote: str) -> str:
+        if (case_data := await self.get_current_case(user)) is None:
+            return "case not selected"
+        return "OK"
