61fb7be3021d39772002449efde06664ab3e3107
Czertilla <blaztermapskorsak@gmail.com>
2024-03-27 20:57:35 +0300

    feature(schemas/specialists) add SCaseGet class add class SCaseGet

--- src/schemas/specialists.py
+++ src/schemas/specialists.py
@@ -1,13 +1,25 @@
-from typing import Annotated
-from fastapi import Query, UploadFile
-from pydantic import BaseModel
-from models.cases import CaseStatus
-from schemas.cameras import SCaseInsert
 
-class SGetCaseResponce(BaseModel):
+from datetime import datetime
+from typing import Annotated
+from fastapi import Query
+from pydantic import BaseModel, Field
+
+
+class SCaseGet(BaseModel):
+    transport: Annotated[str, Field(pattern="[0-9][A-Z]{3}[0-9]{4}")]
+    violation_type: str
+    violation_value: Annotated[str, Field(max_length=100)]
+    case_timestamp: datetime
+    
+    class Config:
+        from_atributes = True
+
+class SGetCaseResponse(BaseModel):
     photo: bytes
-    metadata: SCaseInsert
-    ...
+    metadata: SCaseGet
+
+    class Config:
+        from_atributes = True
 
 class SVoteRequest(BaseModel):
     vote: Annotated[str, Query(pattern=f"justify|convinct")]
