fab8634dc4653a13cccb255c8d828563180f2722
Czertilla <blaztermapskorsak@gmail.com>
2024-03-27 21:02:52 +0300

    feature(CaseService) add & update methods.. update module services.cases:     update class CaseService:         add method get_case_response         update method get_random_case         update method vote

--- src/services/cases.py
+++ src/services/cases.py
@@ -1,11 +1,13 @@
 from uuid import UUID
 from models.cases import CaseORM
 from models.users import UserORM
+from models.votes import VoteORM
 from schemas.cameras import SCameraCase, SCaseInsert
+from schemas.specialists import SCaseGet, SGetCaseResponse
 from services.dynamic import CamerusService
 from utils.absract.service import BaseService
 from utils.requests import deserialize
-from random import choice
+from secrets import choice
 
 
 class CaseService(BaseService):
@@ -23,11 +25,10 @@ class CaseService(BaseService):
             case_data.update(
                 {"photo_id": await self.uow.files.upload_bytes(byte_array)}
             )
+            # print(case_data.get("photo_id"))
             model = SCaseInsert.model_validate(case_data).model_dump()
             result = await self.uow.cases.add_one(model)
             await self.uow.commit()
-        return result
-        
 
 
     async def get_current_case(self, user: UserORM) -> CaseORM|None:
@@ -36,20 +37,46 @@ class CaseService(BaseService):
                 return vote.case_model
 
 
-    async def get_random_case(self, user: UserORM) -> CaseORM|None:
+    async def get_case_response(self, case_model: CaseORM) -> SGetCaseResponse:
+        async with self.uow:
+            photo = await self.uow.files.download_bytes(case_model.photo_id)
+            violation = await self.uow.violations.find_by_id(case_model.violation_id)
+            violation_type = getattr(violation, "violation_type", "unknow")
+            violation_val = getattr(violation, "violation_value", "")
+        return SGetCaseResponse(
+            photo=photo,
+            metadata=SCaseGet(
+                transport=case_model.transport,
+                violation_type=violation_type,
+                case_timestamp=case_model.case_timestamp,
+                violation_value=violation_val
+            )
+        )
+
+    async def get_random_case(self, user: UserORM) -> SGetCaseResponse|None:
         async with self.uow:
             user = await self.uow.users.get(user.id)
-            if (case_data:= await self.get_current_case(user)) is None:
-                cases_list = await self.uow.cases.find_new_for_vote(user)
-            else:
-                return case_data
-            case_model: CaseORM = choice(cases_list)
-            case_model.users_list.append(user)
-            await self.uow.cases.merge(case_model, flush=True)
-        return case_model
+            case_model = await self.get_current_case(user)
+            if case_model is None:
+                cases_list = await self.uow.cases.find_new_for_vote(
+                    user.skill, [cm.id for cm in user.cases_list])
+                if not cases_list:
+                    return
+                case_model: CaseORM = choice(cases_list)
+                vote = VoteORM(case_id=case_model.id, user_id=user.id)
+                case_model.votes_list.append(vote)
+                await self.uow.cases.merge(case_model, flush=True)
+            await self.uow.commit()
+        return await self.get_case_response(case_model)
 
 
-    async def vote(self, user: UserORM, vote: str) -> str:
-        if (case_data := await self.get_current_case(user)) is None:
-            return "case not selected"
-        return "OK"
+    async def vote(self, user: UserORM, justify: bool) -> str|UUID:
+        async with self.uow:
+            user = await self.uow.users.get(user.id)
+            case_model = await self.get_current_case(user)
+            if case_model is None:
+                return "case not selected"
+            await self.uow.cases.vote(case_model.id, user.id, justify)
+            result = case_model.id
+            await self.uow.commit()
+        return result
