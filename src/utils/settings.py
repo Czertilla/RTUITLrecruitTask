627bf450cc05d5119ad820c4074ca77d53855b67
Czertilla <blaztermapskorsak@gmail.com>
2024-03-28 23:19:20 +0300

    feature(settings) add K field update module src.utils.settings:     update class Settings:         add static field K

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -14,6 +14,7 @@ class Settings(BaseSettings):
     DB_DBMS: str = "sqlite"
     USERS_SECTRET: str
     PASSW_SECTRET: str
+    K: int
 
     model_config = SettingsConfigDict(env_file=environ, extra="ignore")
 
